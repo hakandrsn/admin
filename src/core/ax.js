d25f8edce546530351b020035cd40308bbe1f49e
Hakan D <68669489+hakandrsn@users.noreply.github.com>
2022-06-21 17:41:43 +0300

    update1

--- src/core/ax.js
+++ src/core/ax.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: process.env.REACT_APP_URL,
+    baseURL: process.env.REACT_APP_PROD_URL,
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("token") ? localStorage.getItem("token") : ""}`,
